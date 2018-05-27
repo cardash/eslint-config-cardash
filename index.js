d3b47e8d0a22de90047ab5d4d22bfa9c8c582838
Paul Hoang <paulkhoahoang@gmail.com>
2018-05-26 19:28:18 -0700

    Fix config

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 module.exports = {
-  extends: 'airbnb',
+  extends: 'airbnb-base',
   parser: 'babel-eslint',
   plugins: [
-    'react',
-    'jsx-a11y',
     'import'
   ],
   globals: {
@@ -41,25 +39,16 @@ module.exports = {
       exports: 'always',
       functions: 'ignore',
     }],
-    /* it's unfortunate that we need this rule... but Safari and mobile Safari
-    are extremely limiting in what you can do with `button` components */
-    'jsx-a11y/no-static-element-interactions': 0,
-    // TODO this rule shouldn't be necessary, but there appears to be a bug
-    // with jsx-a11y. when that's resolved, this can be removed as a rule
-    'jsx-a11y/href-no-hash': 'off',
     /* package.json has been deduped, which triggers this on most packages. We
        will disable this rule for now while we figure out a good solution. */
     'import/no-extraneous-dependencies': 0,
     semi: [2, 'never'],
     'arrow-body-style': 0,
     quotes: ['error', 'single'],
-    'react/jsx-closing-bracket-location': [1, 'after-props'],
-    'react/jsx-filename-extension': [1, { extensions: ['.js'], }],
     'global-require': 0,
     'no-plusplus': 0,
     'no-param-reassign': [2, { props: false, }],
     'no-mixed-operators': 0,
     'no-continue': 0,
-    'react/forbid-prop-types': 0,
   },
 }
